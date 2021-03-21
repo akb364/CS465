d47ea6c588f0965cd23836b062f63136df86e539
EDonnelly24 <61127769+EDonnelly24@users.noreply.github.com>
2021-03-21 15:44:39 -0700

    Add files via upload

--- TransactionSever/LockManager.java
+++ TransactionSever/LockManager.java
@@ -1,13 +1,30 @@
+import java.util.Enumeration;
+import java.util.Hashtable;
+
 public class LockManager {
-    
-    public void setLock()
-    {
 
+    private Hashtable theLocks;
+
+    public void setLock(Object object, Trans trans, LockType lockType)
+    {
+        Lock foundLock;
+        synchronized(this) {
+            // find the lock associated with object
+            // if there isnt one, create it and add to the hashtable
+        }
+        foundLock.aquire(trans, lockType);
     }
+    // synchronize this one because we want to remove all entries
 
-    public synchronized void unLock()
+    public synchronized void unLock(TransID trans)
     {
-        
+        Enumeration e = theLocks.elements();
+        while(e.hasMoreElements()) {
+            Lock aLock = (Lock) (e.nextElement());
+            if( /* trans is a holder of this lock*/) {
+                aLock.release(trans);
+            }
+        }
     }
 
 }
