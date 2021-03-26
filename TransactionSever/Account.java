b44670bef940fc5d7e73d6ddb3fc94012273cfaa
ksisant3 <33877016+ksisant3@users.noreply.github.com>
2021-03-25 18:54:25 -0700

    added account and accountManager code

--- TransactionSever/Account.java
+++ TransactionSever/Account.java
@@ -1,13 +1,23 @@
 public class Account {
-    
-    public class Account()
-    {
 
+    private int balance;
+    private final int accountNum;
+    
+    public Account(int num, int initialBalance)
+    {
+        this.balance = initialBalance;
+        this.accountNum = num;
     }
 
     public int getBalance()
     {
+        return this.balance;
+    }
 
+    public int setBalance(int newBalance)
+    {
+        this.balance = newBalance;
+        return this.balance;
     }
 
     public transferRandAmount()
--- TransactionSever/AccountManager.java
+++ TransactionSever/AccountManager.java
@@ -1,25 +1,61 @@
-public class AccountManager {
+import java.util.ArrayList;
+import transaction.server.transaction.Transaction;
+import transaction.server.TransactionServer;
+import transaction.server.lock.LockTypes;
+
+public class AccountManager implements LockTypes
+{
     
+    private static ArrayList<Account> accounts;
+    static int numberAccounts;
+    static int initialBalance; 
     private lockManager = new LockManager();
 
-    public AccountManager()
+    public AccountManager(int numberAccounts, int initialBalance)
     {
+        accounts = new ArrayList();
+        AccountManager.numberAccounts = numberAccounts;
+        AccountManager.initialBalance = initialBalance;
+        int accountIndex;
 
+        for(accountIndex = 0; accountIndex < numberAccounts; accountIndex++)
+        {
+            accounts.add(accountIndex, new Account(accountIndex, initialBalance));
+        }
     }
 
-    public read()
+    public Account getAccount(int accountNumber)
     {
-
+        return accounts.get(accountNumber);
     }
 
-    public write()
+    public ArrayList<Account> getAccounts()
     {
-
+        return accounts;
     }
 
-    public getAccounts()
+    public int read(int accountNum, Transaction transaction)
     {
+        // get account
+        Account account = getAccount(accountNumber);
+
+        // set read lock and wait until lock is free
+        (TransactionServer.lockManager).lock(account, transaction, READ_LOCK);
+
+        // return when lock is released
+        return (getAccount(accountNumber)).getBalance();
+    }
+
+    public void write(int accountNum, Transaction transaction, int balance)
+    {
+        // get account
+        Account account = getAccount(accountNumber);
+
+        // set write lock and wait until lock is free
+        (TransactionServer.lockManager).lock(account, transaction, WRITE_LOCK);
         
+        // write amount
+        account.setBalance(balance);
     }
 
 }
